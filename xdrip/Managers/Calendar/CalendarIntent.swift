072aad09937ea8c4127fcde532560454f6b62bc8
dugamarian <35396265+dugamarian@users.noreply.github.com>
2024-12-18 19:51:15 +0100

    calendar settings have been moved to shortcuts

--- xdrip/Managers/Calendar/CalendarIntent.swift
+++ xdrip/Managers/Calendar/CalendarIntent.swift
@@ -1,28 +1,103 @@
 import AppIntents
+import EventKit
 
 @available(iOS 16.0, *)
 struct SetCalendarDeliveryIntent: AppIntent {
-    static var title: LocalizedStringResource = "Set Calendar Delivery"
-
+    static var title: LocalizedStringResource = "Start Calendar Delivery"
+    
     static var description = IntentDescription(
-        "Enable or disable the delivery of data to the calendar.",
+        "Configure and enable the delivery",
         categoryName: "Calendar Management"
     )
-
+    
     @Parameter(title: "Enable Calendar Delivery")
     var isEnabled: Bool
+    
+    @Parameter(title: "Calendar", default: nil)
+    var calendar: CalendarEntity
 
+    @Parameter(title: "Trend", default: false)
+    var displayTrendInCalendarEvent: Bool
+
+    @Parameter(title: "Delta", default: false)
+    var displayDeltaInCalendarEvent: Bool
+
+    @Parameter(title: "Glucose Unit", default: false)
+    var displayUnitInCalendarEvent: Bool
+
+    @Parameter(title: "Interval", default: 0)
+    var calendarInterval: Int
+
+    @Parameter(title: "Visual Indicator", default: false)
+    var displayVisualIndicatorInCalendarEvent: Bool
+    
     @MainActor
     func perform() async throws -> some IntentResult {
         let coreDataManager = await CoreDataManager.create(for: ConstantsCoreData.modelName)
         let calendarManager = CalendarManager(coreDataManager: coreDataManager)
         
+        UserDefaults.standard.calenderId = calendar.id
+        UserDefaults.standard.displayTrendInCalendarEvent = displayTrendInCalendarEvent
+        UserDefaults.standard.displayDeltaInCalendarEvent = displayDeltaInCalendarEvent
+        UserDefaults.standard.displayUnitInCalendarEvent = displayUnitInCalendarEvent
+        UserDefaults.standard.calendarInterval = calendarInterval
+        UserDefaults.standard.displayVisualIndicatorInCalendarEvent = displayVisualIndicatorInCalendarEvent
+
         if isEnabled {
             calendarManager.startCalendarDelivery()
-            return .result()
         } else {
             calendarManager.stopCalendarDelivery()
-            return .result()
         }
+        
+        return .result()
+    }
+}
+
+@available(iOS 16.0, *)
+struct StopCalendarDeliveryIntent: AppIntent {
+    static var title: LocalizedStringResource = "Stop Calendar Delivery"
+    static var description = IntentDescription(
+        "Stop delivering data to the calendar.",
+        categoryName: "Calendar Management"
+    )
+    
+    @MainActor
+    func perform() async throws -> some IntentResult {
+        let coreDataManager = await CoreDataManager.create(for: ConstantsCoreData.modelName)
+        let calendarManager = CalendarManager(coreDataManager: coreDataManager)
+        
+        calendarManager.stopCalendarDelivery()
+        
+        return .result()
+    }
+}
+
+struct CalendarEntity: AppEntity {
+    static var defaultQuery = CalendarQuery()
+    
+    static var typeDisplayRepresentation: TypeDisplayRepresentation {
+        "Calendar"
+    }
+    
+    var id: String
+    var name: String
+    
+    var displayRepresentation: DisplayRepresentation {
+        DisplayRepresentation(title: "\(name)")
+    }
+}
+
+struct CalendarQuery: EntityQuery {
+    func entities(for identifiers: [String]) async throws -> [CalendarEntity] {
+        let eventStore = EKEventStore()
+        let allCalendars = eventStore.calendars(for: .event)
+        let filtered = allCalendars.filter { identifiers.contains($0.calendarIdentifier) }
+        return filtered.map { CalendarEntity(id: $0.calendarIdentifier, name: $0.title) }
+    }
+    
+    func suggestedEntities() async throws -> [CalendarEntity] {
+        let eventStore = EKEventStore()
+        let allCalendars = eventStore.calendars(for: .event)
+        return allCalendars.map { CalendarEntity(id: $0.calendarIdentifier, name: $0.title) }
     }
 }
